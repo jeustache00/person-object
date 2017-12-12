b76314eb1c5f1dbf4ad9f4d1e9c3ca73e2f98ba8
Julie Eustache <jeustache90@yahoo.com>
2017-12-11 19:33:51 -0500

    first commit

---
+++ palindromechallenge.js
@@ -0,0 +1,11 @@
+// function checksPal(x){
+//   if(x.charAt(0)(x.length-1) && x.charAt(1)==x.charAt(x.length-2)){
+//     alert("Palindrome!")
+//   }else{
+//     alert("NOT!")
+//   }
+// }
+// checksPal("madam");
+
+// determine if any string passed within a function is a palindrome
+// for loop
